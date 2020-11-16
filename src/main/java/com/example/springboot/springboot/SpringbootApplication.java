800e26cc7e5b3cba23b5ec93b7d0d475ac82a8d9
sait <kromsait@gmail.com>
2020-11-16 23:59:26 +0300

    bizibisalaq

---
+++ src/main/java/com/example/springboot/springboot/SpringbootApplication.java
@@ -0,0 +1,17 @@
+
+package com.example.springboot.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootApplication {
+
+	public static void main(String[] args) {
+		System.out.print("baba");
+		SpringApplication.run(SpringbootApplication.class, args);
+		
+
+	}
+
+}
